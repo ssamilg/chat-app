ebfd6b96abd4ba8b5141935cc442c3cfc2bde506
ssamilg <samil-98@hotmail.com>
2021-05-02 05:55:19 +0300

    chat conversation component initial design added

--- client/src/views/components/ChatConversation.vue
+++ client/src/views/components/ChatConversation.vue
@@ -12,6 +12,11 @@ export default {
   computed: {
     ...mapGetters(['user', 'activeChat', 'activeConversation']),
   },
+  methods: {
+    isSentByMe(message) {
+      return message.messageFrom === this.user.username;
+    },
+  },
 };
 </script>
 
@@ -27,11 +32,29 @@ export default {
       </v-flex>
     </v-layout>
 
-    <v-layout
-      v-for="message in activeConversation.messages"
-      :key="message.id"
-    >
-      {{ message }}
+
+    <v-layout class="conversation-content">
+      <v-flex>
+        <v-layout
+          v-for="message in activeConversation.messages"
+          :key="message.id"
+          class="pt-5"
+        >
+          <v-flex>
+            <v-layout
+              :reverse="isSentByMe(message)"
+            >
+              {{ message.messageFrom }}
+            </v-layout>
+
+            <v-layout
+              :reverse="isSentByMe(message)"
+            >
+              {{ message.content }}
+            </v-layout>
+          </v-flex>
+        </v-layout>
+      </v-flex>
     </v-layout>
   </div>
 </template>
@@ -39,14 +62,21 @@ export default {
 <style lang="scss">
 #chat-conversation {
   height: 100%;
+  background-color: #F4F0F9;
 
-  .conversation-header {
-    background-color: #D1C4E9;
-    height: 48px;
-    font-size: 20px;
-    font-weight: 600;
-    padding-left: 8px;
-    text-transform: capitalize;
+  .conversation {
+    &-header {
+      background-color: #D1C4E9;
+      height: 48px;
+      font-size: 20px;
+      font-weight: 600;
+      padding-left: 8px;
+      text-transform: capitalize;
+    }
+
+    &-content {
+      padding: 0 8px;
+    }
   }
 }
 </style>
