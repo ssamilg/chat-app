bfc9c5e6609d1556abd8e41706abf3c9d2f4c751
ssamilg <samil-98@hotmail.com>
2021-05-05 00:32:35 +0300

    message bubble design implemented

--- client/src/views/components/MessageBubble.vue
+++ client/src/views/components/MessageBubble.vue
@@ -8,6 +8,14 @@ export default {
       type: Object,
       required: true,
     },
+    prevMessage: {
+      type: Object,
+      required: true,
+    },
+    nextMessage: {
+      type: Object,
+      required: true,
+    },
     isRoomMessage: {
       type: Boolean,
       required: true,
@@ -18,21 +26,60 @@ export default {
     isSentByMe() {
       return this.message.messageFrom === this.user.username;
     },
+    isSequel() {
+      return this.prevMessage.messageFrom === this.message.messageFrom;
+    },
+    bubbleClass() {
+      let messageBubbleClass = 'message-bubble';
+
+      if (this.isSentByMe) {
+        messageBubbleClass += ' sender';
+
+        if (this.isSequel) {
+          messageBubbleClass += ' sender-sequel';
+        }
+      } else {
+        messageBubbleClass += ' receiver';
+
+        if (this.isSequel) {
+          messageBubbleClass += ' receiver-sequel';
+        }
+      }
+
+      return messageBubbleClass;
+    },
+    messageTime() {
+      const messageDate = new Date(this.message.dateSend);
+
+      return `${messageDate.getHours()}.${messageDate.getMinutes()}`;
+    },
   },
 };
 </script>
 
 <template>
   <div id="message-bubble-wrapper">
-    <v-layout :reverse="isSentByMe">
-      <v-flex shrink class="message-bubble">
-        <v-layout v-if="isRoomMessage" :class="isSentByMe ? 'justify-end' : ''">
-          {{ message.messageFrom }}
+    <v-layout align-center :reverse="isSentByMe">
+      <v-flex shrink :class="bubbleClass">
+        <v-layout v-if="!isSequel" :class="isSentByMe ? 'justify-end' : ''">
+          <v-flex shrink class="message-sender">
+            {{ message.messageFrom }}
+          </v-flex>
         </v-layout>
 
-        <v-layout>
-          {{ message.content }}
+        <v-layout :reverse="isSentByMe">
+          <v-flex shrink>
+            {{ message.content }}
+          </v-flex>
         </v-layout>
+      </v-flex>
+
+      <v-icon size="20" class="message-time px-1">
+        mdi-circle-medium
+      </v-icon>
+
+      <v-flex shrink px-1 class="message-time">
+        {{ messageTime }}
       </v-flex>
     </v-layout>
   </div>
@@ -40,11 +87,44 @@ export default {
 
 <style lang="scss">
 #message-bubble-wrapper {
+  &:hover {
+    .message-time {
+      display: block;
+    }
+  }
 
   .message-bubble {
     padding: 8px;
-    border: 2px solid red;
-    border-radius: 12px;
+    min-width: 10vh;
+
+    .message-sender {
+      font-weight: 600;
+    }
+  }
+
+  .message-time {
+    display: none;
+  }
+
+  .sender {
+    border-radius: 12px 12px 0 12px;
+    background-color: #673AB7;
+    color: white;
+
+    &-sequel {
+      border-radius: 12px 0 0 12px;
+    }
+
+  }
+
+  .receiver {
+    border-radius: 12px 12px 12px 0;
+    background-color: #D1C4E9;
+    color: #311B92;
+
+    &-sequel {
+      border-radius: 0 12px 12px 0;
+    }
   }
 
 }
